30a3b41b8bfed6f19a8e0f09e28b7aa9b521b756
917rafael <rpilger917@gmail.com>
2025-05-20 16:27:47 -0300

    FEAT: Create autor.py

--- app/urls.py
+++ app/urls.py
@@ -8,12 +8,13 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet
-from core.views import CategoriaViewSet
+from core.views import CategoriaViewSet, AutorViewSet
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
+router.register(r'autores', AutorViewSet, basename='autores')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -1,2 +1,3 @@
 from .user import UserViewSet
 from .categoria import CategoriaViewSet
+from .autor import  AutorViewSet
